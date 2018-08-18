a036fd90ddf2a118d07589f12d5b4a73a78c6f9a
Mukul Latiyan <subhanshu.25197@gmail.com>
2018-08-18 23:40:30 +0530

    day1 final

--- Day1/app.js
+++ Day1/app.js
@@ -1,71 +1,49 @@
 //working with console
 
-console.log(123)
+console.log(123) //console numbers
 
-console.log('hello world')
+console.log('hello') //console strings
 
-console.log(true)
+console.log(true) //console boolean values
 
-//var greeting = "hello"
+console.log([1,2,3,4,5]) // console an array
 
-console.log(greeting)
+console.log({name:'Mukul',age:'21'}) //console objects
 
-console.log([1,3,4,5,6])
+console.table({name:'John Doe',age:'21'})//console object in form of a table
 
-console.log({a:1,b:2})
+var myArray=[{a:1,b:2,c:3},{a:1,b:2,c:3,d:4},{k:11,f:22},{a:1,b:2,c:3}]
 
-console.table({a:1,b:2})
+console.table(myArray)//console array in form of a table
 
-console.error('This is some error')
+console.error('this is an error. duhh!') //console an error
 
-console.warn('This is a warning')
+console.warn('this is a warning') //console a warning
 
-console.clear();
+console.time('hello') // find the time execution of a code block
+    console.log('this')
+    console.log('will')
+    console.log('tell')
+    console.log('you')
+    console.log('about')
+    console.log('the')
+    console.log('time')
+    console.log('spent')
+    console.log('to')
+    console.log('run')
+    console.log('all')
+    console.log('these')
+    console.log('console.logs')
+console.timeEnd('hello')
 
-console.time('Hello')
-    console.log('ahahahaha')
-    console.log('ahahahaha')
-console.timeEnd('Hello')
+//Convert Your Browser Into An Editor
+document.body.contentEditable = //true(remove the comment)
 
-//var, let, const
+//select dom elements
+console.log(document.querySelector('h1').textContent)
 
-var name = 'Mukul Latiyan'
+//change dom elements
+document.querySelector('p').innerHTML = 'hell yeah! this is awesome!'
 
-name = 'Jyoti Dagar'
-
+// clear you console
 console.clear()
-
-console.log(name)
-
-var greeting
-greeting = ' was beautiful'
-
-// can start with $ or _ (nothing else)
-console.log(name +greeting)
-
-var firstName = 'Mukul'
-var lastName = 'latiyan'
-var fullName = firstName +' '+ lastName
-console.log(fullName)
-
-
-//Const 
-
-//const name = 'Mukul'
-console.log(name)
-
-//const hero
-//name = 'latiyan'
-
-const object = {
-    name : 'Mukul',
-    age: '20'
-}
-
-object.name = 'Rahul'
-object.age = 30
-console.log(object)
-
-const numbers = [1,2,3,4]
-numbers.push(6)
-console.log(numbers)
